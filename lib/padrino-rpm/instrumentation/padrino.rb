c086f7dc5ecfa2081977031d55d06dd3357a94bd
Taras Kunch <taras.kunch@gmail.com>
2012-09-19 23:33:26 +0300

    Improve instrumentation for padrino. Fix routes.

--- lib/padrino-rpm/instrumentation/padrino.rb
+++ lib/padrino-rpm/instrumentation/padrino.rb
@@ -1,38 +1,89 @@
 require 'new_relic/agent/instrumentation/controller_instrumentation'
 
 DependencyDetection.defer do
+  @name = :padrino_controller
   depends_on do
     defined?(::Padrino)
   end
-  
+
   executes do
-    Padrino::Application.class_eval do
-      include PadrinoRpm::Instrumentation::Padrino
+    ::Padrino::Application.class_eval do
+      include PadrinoRpm::Instrumentation::PadrinoController
       alias dispatch_without_newrelic dispatch!
       alias dispatch! dispatch_with_newrelic
     end
   end
 end
 
+DependencyDetection.defer do
+  @name = :padrino_view
+
+  depends_on do
+    defined?(::Padrino)
+  end
+
+  executes do
+    NewRelic::Agent.logger.debug 'Installing Padrino view instrumentation'
+  end
+
+  executes do
+    ::Padrino::Application.class_eval do
+      include PadrinoRpm::Instrumentation::PadrinoView
+      alias render_without_newrelic_trace render
+      alias render render_with_newrelic_trace
+    end
+  end
+end
+
 module PadrinoRpm
   module Instrumentation
-    module Padrino
+    module PadrinoController
       include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-      
+
       def dispatch_with_newrelic(*args, &block)
-        if @route
-          name = @route.as_options[:name]
-          short_name = name.to_s.split(/_/).last
-          controller = @route.controller
-        end
-        
-        perform_action_with_newrelic_trace(:category => :controller, :name => short_name, :params => request.params, :class_name => controller)  do
-          dispatch_without_newrelic#(*args, &block) # RPM loads the sinatra plugin too eagerly
+        route_info = recognize_route_for_newrelic
+        perform_action_with_newrelic_trace({category: :controller, params: request.params}.merge(route_info)) do
+          dispatch_without_newrelic
         end
       end
 
-    end #PadrinoRpm::Instrumentation::Padrino
+      def recognize_route_for_newrelic
+        route_info = {
+          name: '(unknown)',
+          class_name: 'Unknown'
+        }
+
+        recognized_route = self.class.router.recognize(@env)
+        if recognized_route
+          route_name = recognized_route.first.path.route.named.to_s
+          route_info[:name] = route_name
+          route_info[:class_name] = route_name.split('_').first.capitalize
+        #else
+          #logger.warn "ROUTE NOT RECOGNIZED #{@env['PATH_INFO']}"
+        end
+
+        #logger.debug "New Relic route: #{route_info}"
+
+        route_info
+      end
+    end
+
+    module PadrinoView
+      def render_with_newrelic_trace(*args, &block)
+        engine, file = *args
+        return render_without_newrelic_trace(*args, &block) if file == "= yield"
+
+        file = "Proc" if file.is_a?(Proc)
+        metrics = ["View/#{engine}/#{file.to_s.gsub(/\A\/+/, '')}/Rendering"]
+
+        self.class.trace_execution_scoped metrics do
+          render_without_newrelic_trace(*args, &block)
+        end
+      end
+    end
   end
 end
 
 DependencyDetection.detect!
+
+
